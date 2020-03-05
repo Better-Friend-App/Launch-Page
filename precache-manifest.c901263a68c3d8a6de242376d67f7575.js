1329f21643c1a8379f5e1d8c4f86e8f5bd26f9cf
JakeRoyRandall <jake@jakerandall.me>
2020-03-04 22:46:23 -0800

    Updates

---
+++ precache-manifest.c901263a68c3d8a6de242376d67f7575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68095946f7e06589a65a615b0aeefbfc",
+    "url": "/Launch-Page/index.html"
+  },
+  {
+    "revision": "4c526ee7f4a61b97bc18",
+    "url": "/Launch-Page/static/css/main.786a9edc.chunk.css"
+  },
+  {
+    "revision": "afcb8f9098a773ec2a41",
+    "url": "/Launch-Page/static/js/2.6fad0dac.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/Launch-Page/static/js/2.6fad0dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c526ee7f4a61b97bc18",
+    "url": "/Launch-Page/static/js/main.a02ea171.chunk.js"
+  },
+  {
+    "revision": "071cc9af2650b5a2644e",
+    "url": "/Launch-Page/static/js/runtime-main.167f960c.js"
+  }
+]);
